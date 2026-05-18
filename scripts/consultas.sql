f1d5989071f0c5e8a4183bfd782498e353c528e2
Juan Cruz <olguinjuacruz@gmail.com>
2026-05-18 15:58:26 -0300

    Cunsulta 3

--- scripts/consultas.sql
+++ scripts/consultas.sql
@@ -27,9 +27,18 @@ JOIN (
     FROM moliendas
     WHERE cultivo ILIKE 'trigo%'
     GROUP BY anio
-    ) AS m ON co.anio = m.anio AND co.cultivo = 'trigo'
+) AS m ON co.anio = m.anio AND co.cultivo = 'trigo'
 GROUP BY co.anio, co.cultivo, cantidad_molienda_total
 ORDER BY co.anio;
 
 -- CONSULTA 3
--- Comparar los valores obtenidos anteriormente con las superficie cosechadas de ambos cereales y ver la relacion de cual es mayor, menor, etc
+-- TOP 10 departamentos con mayor producción de maíz en el último año registrado (2017)
+SELECT
+    co.anio
+    co.cultivo
+    SUM(co.produccion_tm) AS produccion_total
+FROM cosechas co
+JOIN departamentos de ON co.departamento_id = de.id
+GROUP BY de.nombre
+ORDER BY SUM(co.produccion_tm) DESC
+LIMIT 10;
